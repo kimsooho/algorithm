68bedac3d84a1ac0bbdc85f8c5dd30454ed404d6
kimsooho <kimsh9167@naver.com>
2021-01-28 15:13:28 +0900

    Create baekjoon_7568.cpp
    baekjoon 7568 clear

---
+++ baekjoon/baekjoon_7568.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector<pair<int, int>> v;
+  int N, x, y;
+
+  cin >> N;
+  for(int i=0; i<N; ++i)
+  {
+    cin >> x >> y;
+    v.push_back(make_pair(x, y));
+  }
+
+  for(vector<pair<int, int>>::iterator iter = v.begin(); iter != v.end(); ++iter)
+  {
+    int count = N;
+    for(vector<pair<int, int>>::iterator iter_ = v.begin(); iter_ != v.end(); ++iter_)
+    {
+      if(iter == iter_)
+        continue;
+
+      if(iter->first >= iter_->first || iter->second >= iter_->second) --count;
+    }
+
+    cout << count << " ";
+  }
+}
