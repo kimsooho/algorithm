01d3eadd31e3d50e29e532fe561a712f6bac0f4f
kimsooho <kimsh9167@naver.com>
2021-02-02 00:49:53 +0900

    Create baekjoon_1920.cpp
    baekjoon 1920 clear

---
+++ baekjoon/baekjoon_1920.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int search(int);
+int main()
+{
+  cin.tie(0);
+  cin.sync_with_stdio(false);
+
+  int N, M, input;
+
+  cin >> N;
+  while (N--)
+  {
+    cin >> input;
+    v.push_back(input);
+  }
+  sort(v.begin(), v.end());
+
+  cin >> M;
+
+  while (M--)
+  {
+    cin >> input;
+    cout << search(input) << "\n";
+  }
+}
+
+int search(int num)
+{
+  int l = 0, r = v.size() - 1;
+  while (l <= r)
+  {
+    int m = (l + r) / 2;
+    if (v[m] > num)
+    {
+      r = m - 1;
+    }
+    else if (v[m] < num)
+    {
+      l = m + 1;
+    }
+    else
+    {
+      return 1;
+    }
+  }
+  return 0;
+}
