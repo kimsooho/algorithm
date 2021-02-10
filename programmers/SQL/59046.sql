935a33916211cdb49dba06292ba094f2c0dc5abb
kimsooho <kimsh9167@naver.com>
2021-02-10 15:05:19 +0900

    Create 59046.sql
    Programmers SQL 59046 clear

---
+++ programmers/SQL/59046.sql
@@ -0,0 +1,8 @@
+/*
+  https://programmers.co.kr/learn/courses/30/lessons/59046
+*/
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE 
+FROM ANIMAL_INS
+WHERE NAME = "Lucy" OR NAME = "Ella" OR NAME = "Pickle" OR NAME = "Rogan" OR NAME = "Sabrina" OR NAME = "Mitty"
+ORDER BY ANIMAL_ID;
