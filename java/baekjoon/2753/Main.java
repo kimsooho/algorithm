c819d8a16b2d13fb9b3e8f0766193fc263a065b3
kimsuho <kimsh9167@naver.com>
2021-08-26 19:23:04 +0900

    [JAVA_BJ] 2753 clear

---
+++ java/baekjoon/2753/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author suho kim
+ * @no 2753
+ * @title 빵집
+ * @url https://www.acmicpc.net/problem/2743
+ * @date 2021.08.26
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.nextLine().length());
+  }
+}
