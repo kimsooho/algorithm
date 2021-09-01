d70f4ef67d0fda927d07a31b6d02a0dd9d82ff7a
kimsuho <kimsh9167@naver.com>
2021-09-01 18:59:54 +0900

    [JAVA_swexpert] Add Comment

--- java/swexpert/10912/Solution.java
+++ java/swexpert/10912/Solution.java
@@ -1,3 +1,10 @@
+
+/**
+* @author suho kim
+* @no 10912
+* @title 외로운 문자
+* @date 2021.08.18
+*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,38 +17,32 @@ public class Solution {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int T = Integer.parseInt(br.readLine());
 
-    for(int t=1; t<=T; ++t)
-    {
+    for (int t = 1; t <= T; ++t) {
       ArrayList<String> arrStr = new ArrayList<String>();
       HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
       String line = br.readLine();
 
-      for(int i=0; i<line.length(); ++i)
-      {
+      for (int i = 0; i < line.length(); ++i) {
         String str = String.valueOf(line.charAt(i));
-        if(!hashMap.containsKey(str))
-        {
+        if (!hashMap.containsKey(str)) {
           hashMap.put(str, 0);
         }
 
-        hashMap.replace(str, hashMap.get(str)+1);
+        hashMap.replace(str, hashMap.get(str) + 1);
       }
 
-      for(String key : hashMap.keySet())
-      {
-        if(hashMap.get(key) % 2 != 0)
-        {
+      for (String key : hashMap.keySet()) {
+        if (hashMap.get(key) % 2 != 0) {
           arrStr.add(key);
         }
       }
 
       Collections.sort(arrStr);
       StringBuilder sb = new StringBuilder();
-      for(String s : arrStr)
-      {
+      for (String s : arrStr) {
         sb.append(s);
       }
-      
+
       System.out.println(String.format("#%d %s", t, arrStr.isEmpty() ? "Good" : sb));
     }
   }
