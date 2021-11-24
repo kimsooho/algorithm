f781bfa3bf2df2e798d2c38094e7edf581867444
kimsuho <kimsh9167@naver.com>
2021-11-24 19:16:50 +0900

    [JAVA_BJ] 10926 clear

---
+++ java/baekjoon/10926/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author suho kim
+ * @no 10926
+ * @title ??!
+ * @url https://www.acmicpc.net/problem/10926
+ * @date 2021.11.24
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+
+    System.out.println(input + "??!");
+  }
+}
