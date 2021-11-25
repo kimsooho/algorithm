bede6818af842d56546a37bd223853e1f37a5fce
kimsuho <kimsh9167@naver.com>
2021-11-25 21:06:02 +0900

    [JAVA_BJ] 5338 clear

---
+++ java/baekjoon/5338/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author suho kim
+ * @no 5338
+ * @title 마이크로소프트 로고
+ * @url https://www.acmicpc.net/problem/5338
+ * @date 2021.11.25
+ */
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(
+        "       _.-;;-._\n" + "'-..-'|   ||   |\n" + "'-..-'|_.-;;-._|\n" + "'-..-'|   ||   |\n" + "'-..-'|_.-''-._|");
+
+    System.out.println(sb);
+  }
+}
