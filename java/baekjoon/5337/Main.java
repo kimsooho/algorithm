b09401753dfc53ffec830732a271617337f88993
kimsuho <kimsh9167@naver.com>
2021-11-27 17:29:21 +0900

    [JAVA_BJ] 5337

---
+++ java/baekjoon/5337/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author suho kim
+ * @no 5337
+ * @title 웰컴
+ * @url https://www.acmicpc.net/problem/5337
+ * @date 2021.11.27
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(".  .   .");
+    System.out.println("|  | _ | _. _ ._ _  _");
+    System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+  }
+}
