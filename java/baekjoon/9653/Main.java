b36fb3a3352bcbf439620dfa6369038f6322de86
kimsuho <kimsh9167@naver.com>
2021-11-28 22:01:07 +0900

    [JAVA_BJ] 9653 clear

---
+++ java/baekjoon/9653/Main.java
@@ -0,0 +1,22 @@
+/**
+ * @author suho kim
+ * @no 9653
+ * @title 스타워즈 로고
+ * @url https://www.acmicpc.net/problem/9653
+ * @date 2021.11.28
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("    8888888888  888    88888");
+    System.out.println("   88     88   88 88   88  88");
+    System.out.println("    8888  88  88   88  88888");
+    System.out.println("       88 88 888888888 88   88");
+    System.out.println("88888888  88 88     88 88    888888");
+    System.out.println("");
+    System.out.println("88  88  88   888    88888    888888");
+    System.out.println("88  88  88  88 88   88  88  88");
+    System.out.println("88 8888 88 88   88  88888    8888");
+    System.out.println(" 888  888 888888888 88  88      88");
+    System.out.println("  88  88  88     88 88   88888888");
+  }
+}
