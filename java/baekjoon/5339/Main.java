2cbbbe00bea0da780d7b5705f469aaa9cd024682
kimsuho <kimsh9167@naver.com>
2021-11-29 18:09:10 +0900

    [JAVA_BJ] 5339 clear

---
+++ java/baekjoon/5339/Main.java
@@ -0,0 +1,21 @@
+/**
+ * @author suho kim
+ * @no 5339
+ * @title 콜센터
+ * @url https://www.acmicpc.net/problem/5339
+ * @date 2021.11.29
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("     /~\\");
+    System.out.println("    ( oo|");
+    System.out.println("    _\\=/_");
+    System.out.println("   /  _  \\");
+    System.out.println("  //|/.\\|\\\\");
+    System.out.println(" ||  \\ /  ||");
+    System.out.println("============");
+    System.out.println("|          |");
+    System.out.println("|          |");
+    System.out.println("|          |");
+  }
+}
