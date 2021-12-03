2557e080b518a10fdb68006eeb6737651c20c005
kimsuho <kimsh9167@naver.com>
2021-12-03 18:37:34 +0900

    [JAVA_BJ] 14645 clear

---
+++ java/baekjoon/14645/Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author suho kim
+ * @no 14645
+ * @title 와이버스 부릉부릉
+ * @url https://www.acmicpc.net/problem/14645
+ * @date 2021.12.03
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    sc.nextLine();
+    sc.nextLine();
+    sc.nextLine();
+    sc.nextLine();
+    System.out.println("비와이");
+  }
+}
