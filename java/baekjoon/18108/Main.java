822bc037dac27a63fd05a2d4c3f3627c7f9c29cc
kimsuho <kimsh9167@naver.com>
2021-12-05 23:16:20 +0900

    [JAVA_BJ] 18108 clear

---
+++ java/baekjoon/18108/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author suho kim
+ * @no 18108
+ * @title 1998년생인 내가 태국에서는 2541년생?!
+ * @url https://www.acmicpc.net/problem/18108
+ * @date 2021.12.05
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int y = sc.nextInt();
+    System.out.println(y - 543);
+  }
+}
