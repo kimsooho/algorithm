2db4f4f06c867365d7a3ed1709d68ab97714f7e5
kimsuho <kimsh9167@naver.com>
2021-12-05 22:26:16 +0900

    [JAVA_BJ] 16394 clear

---
+++ java/baekjoon/16394/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author suho kim
+ * @no 16394
+ * @title 홍익대학교
+ * @url https://www.acmicpc.net/problem/16394
+ * @date 2021.12.04
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+    System.out.println(year - 1946);
+  }
+}
