81550c42dbd4ca24b6fe2708c360e2f9ebcd104c
kimsuho <kimsh9167@naver.com>
2021-12-09 15:09:24 +0900

    [JAVA_BJ] 6749 clear

---
+++ java/baekjoon/6749/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author suho kim
+ * @no 6749
+ * @title Next in line
+ * @url https://www.acmicpc.net/problem/6749
+ * @date 2021.12.09
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int Y = sc.nextInt();
+    int M = sc.nextInt();
+
+    System.out.println(M + M - Y);
+  }
+}
