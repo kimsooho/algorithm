04de1ceee7b2236a95d68e9527a5f5e3d7dae717
kimsuho <kimsh9167@naver.com>
2021-12-16 21:36:30 +0900

    [JAVA_BJ] 3046 clear

---
+++ java/baekjoon/3046/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author suho kim
+ * @no R2
+ * @title 3046
+ * @url https://www.acmicpc.net/problem/3046
+ * @date 2021.12.16
+ */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int R1 = sc.nextInt();
+    int S = sc.nextInt();
+    System.out.println(S * 2 - R1);
+  }
+}
